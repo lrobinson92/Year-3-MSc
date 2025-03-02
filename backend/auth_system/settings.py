11f1e71f5deef7c2fd1873c4ae55bd3ce0d72acb
Leonie.Robinson <lrobinson92@live.co.uk>
2025-03-02 22:43:29 +0000

    removed line

--- backend/auth_system/settings.py
+++ backend/auth_system/settings.py
@@ -62,7 +62,6 @@ MIDDLEWARE = [
     'django.contrib.sessions.middleware.SessionMiddleware',
     'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
-    
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
